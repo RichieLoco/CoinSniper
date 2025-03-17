2fce3d14af0590c8fcd0d3815c4101588292f352
RichieLoco <richieloco@hotmail.co.uk>
2025-03-17 18:15:36 +0000

    Initial commit - dependencies pulled in via Spring Initializer

---
+++ src/main/java/com/richieloco/coin_sniper/CoinSniperApplication.java
@@ -0,0 +1,13 @@
+package com.richieloco.coin_sniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinSniperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinSniperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richieloco/coin_sniper/CoinSniperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richieloco.coin_sniper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinSniperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
